cdcc58714bf3a3863490ea325faaa66e289909c0
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-02-19 13:48:28 +0000

    refactored main css, header and footer, added parallax effect to top container and added the blur effect to it too

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,8 +8,51 @@ function toggleScrollAnimation(){
   }
 }
 
+function toggleVideoOpacity(){
+  if(window.scrollY>=750){
+    document.getElementById("introVideo").style.filter="blur(1.5em)";
+  }
+  else if(window.scrollY>=725){
+    document.getElementById("introVideo").style.filter="blur(1.375em)";
+  }
+  else if(window.scrollY>=700){
+    document.getElementById("introVideo").style.filter="blur(1.25em)";
+  }
+  else if(window.scrollY>=675){
+    document.getElementById("introVideo").style.filter="blur(1.125em)";
+  }
+  else if(window.scrollY>=650){
+    document.getElementById("introVideo").style.filter="blur(1em)";
+  }
+  else if(window.scrollY>=625){
+    document.getElementById("introVideo").style.filter="blur(.875em)";
+  }
+  else if(window.scrollY>=600){
+    document.getElementById("introVideo").style.filter="blur(.75em)";
+  }
+  else if(window.scrollY>=575){
+    document.getElementById("introVideo").style.filter="blur(.625em)";
+  }
+  else if(window.scrollY>=550){
+    document.getElementById("introVideo").style.filter="blur(.5em)";
+  }
+  else if(window.scrollY>=525){
+    document.getElementById("introVideo").style.filter="blur(.375em)";
+  }
+  else if(window.scrollY>=500){
+    document.getElementById("introVideo").style.filter="blur(.25em)";
+  }
+  else if(window.scrollY>=475){
+    document.getElementById("introVideo").style.filter="blur(.125em)";
+  }
+  else{
+    document.getElementById("introVideo").style.filter="blur(0)";
+  }  
+}
+
 addEventListener("scroll", ()=>{
-  toggleScrollAnimation(); 
+  toggleScrollAnimation();
+  toggleVideoOpacity();
 })
 
 function showRecommendations(){
