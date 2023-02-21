dca6bda6f27d659c870d01f73755406c95edfb2b
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-02-21 00:48:08 +0000

    added services

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -13,10 +13,20 @@ async function copyTextToClipboard(id, skip, id2){
   id2.classList.remove("contact-information-animation");
 }
 
+async function blinkSocialMedia(){
+  document.getElementById("socialMediaLinks").classList.add("social-media-blink");
+  await new Promise(r => setTimeout(r,5000));
+  document.getElementById("socialMediaLinks").classList.remove("social-media-blink");
+}
+
 document.getElementById("copyContainer1").addEventListener("click", ()=>{
   copyTextToClipboard(document.getElementById("copyContainer1"),7, document.getElementById("copyIcon1"));
 })
 
 document.getElementById("copyContainer2").addEventListener("click", ()=>{
   copyTextToClipboard(document.getElementById("copyContainer2"),15, document.getElementById("copyIcon2"));
+})
+
+document.getElementById("socialMediaButton").addEventListener("click", ()=>{
+  blinkSocialMedia()
 })
